ce747a77d1a149ff369be0300c53502602facad5
Valdera <f.valdera@yahoo.co.id>
2022-10-09 23:32:14 +0700

    add cloud spec

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 build:
 	docker build --tag actix_demo .
 
+run:
+	docker run -p 8000:8000 actix_demo 
+
 create-db:
 	./scripts/init_db.sh
+
+migrate-cloud:
+	DATABASE_URL=YOUR-DIGITAL-OCEAN-DB-CONNECTION-STRING sqlx migrate run
+
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,5 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::{
     postgres::{PgConnectOptions, PgSslMode},
     ConnectOptions,
@@ -12,6 +13,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -20,6 +22,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
